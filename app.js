ec7942745bbe3fd3ddfceb85524e0c41171732d8
Andrew Taghi <aataghi13@gmail.com>
2020-07-14 19:54:54 -0500

    data to render on page load

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ class App extends React.Component {
     golf: []
   }
 
-// ===== MAKE A GET REQUEST TO RETRIEVE THE DATA =====
+// ===== MAKE A GET REQUEST TO RETRIEVE THE DATA ====== //
   componentDidMount = () => {
-    axios.get('/fore').then(
+    axios.get('/scorecards').then(
       (response) => {
-        console.log(response);
+        this.setState({
+          golf:response.data
+        })
+        console.log(this.state.golf);
       }
     ).catch(
       (error) => {
@@ -16,9 +19,112 @@ class App extends React.Component {
     )
   }
 
+// ========== CREATE A NEW SCORECARD ========== //
+  createScorecard = (event) => {
+    event.preventDefault();
+    axios.post('/scorecards').then(
+      (response) => {
+        this.setState({
+          date:this.state.newDate,
+          course:this.state.newCourse,
+          teebox:this.state.newTeebox,
+          hole:this.state.newHole,
+          par:this.state.newPar,
+          score:this.state.newScore
+        })
+      }
+    )
+  }
+
+  newDate = (event) => {
+    this.setState({
+      date:event.target.value
+    })
+  }
+
+  newCourse = (event) => {
+    this.setState({
+      course:event.target.value
+    })
+  }
+
+  newTeebox = (event) => {
+    this.setState({
+      teebox:event.target.value
+    })
+  }
+
+  newHole = (event) => {
+    this.setState({
+      hole:event.target.value
+    })
+  }
+
+  newPar = (event) => {
+    this.setState({
+      par:event.target.value
+    })
+  }
+
+  newScore = (event) => {
+    this.setState({
+      score:event.target.value
+    })
+  }
+
   render = () => {
     return (
-      <h1>Hello</h1>
+      <div className="container">
+        <h2>Scorecard</h2>
+        <form>
+          <input type="date"/><br/>
+          <input type="text" placeholder="Course Name"/><br/>
+          <input type="text" placeholder="Teebox"/><br/>
+          <input type="number" placeholder="Hole Number"/><br/>
+          <input type="number" placeholder="Par"/><br/>
+          <input type="number" placeholder="Score"/><br/>
+          <input type="submit" value="Submit"/>
+        </form>
+        <div className="table">
+          {this.state.golf.map((scorecard,index) => {
+            return (
+              <table>
+                <thead>
+                  <tr>
+                    <th colspan="19">Date: {scorecard.day}</th>
+                  </tr>
+                  <tr>
+                    <th colspan="19">Course:{ scorecard.course}</th>
+                  </tr>
+                  <tr>
+                    <th colspan="19">Teebox: {scorecard.teebox}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Hole</th>
+                    {scorecard.hole.map((hole,index) => {
+                      return <th>{hole}</th>
+                    })}
+                  </tr>
+                  <tr>
+                    <th>Par</th>
+                    {scorecard.par.map((par,index) => {
+                      return <th>{par}</th>
+                    })}
+                  </tr>
+                  <tr>
+                    <th>Score</th>
+                    {scorecard.score.map((score,index) => {
+                      return <th>{score}</th>
+                    })}
+                  </tr>
+                </tbody>
+              </table>
+            )
+          })}
+        </div>
+      </div>
     )
   }
 }
